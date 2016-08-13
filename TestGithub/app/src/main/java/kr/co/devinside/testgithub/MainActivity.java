12d02d6731bd050fcaa0d5e9388660ebe008a7d9
DevInside <kecondev@gmail.com>
2016-08-13 15:34:36 +0900

    ^^

---
+++ TestGithub/app/src/main/java/kr/co/devinside/testgithub/MainActivity.java
@@ -0,0 +1,18 @@
+package kr.co.devinside.testgithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/*
+생성일시 : 2016.08.03 생성
+생성자   : 이상용
+수정내용
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
