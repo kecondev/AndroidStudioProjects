ff4d94f98a168bf842d30dddc193ebce74d755b2
DevInside <kecondev@gmail.com>
2017-01-27 14:44:55 +0900

    ^^

---
+++ FragmentExample/app/src/main/java/kr/co/devinside/fragmentexample/ToolbarFragment.java
@@ -0,0 +1,74 @@
+package kr.co.devinside.fragmentexample;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
+
+/**
+ * Created by DevInside on 2017. 1. 15..
+ */
+
+public class ToolbarFragment extends Fragment implements SeekBar.OnSeekBarChangeListener{
+
+    private static int seekvalue = 10;
+    private static EditText edittext;
+
+    ToolbarListener activityCallback;
+
+    public interface ToolbarListener {
+        public void onButtonClick(int position, String text);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try{
+            activityCallback = (ToolbarListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString() + " must implement ToolbarListener");
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState) {
+        View view = inflater.inflate(R.layout.toolbar_fragment, container, false);
+
+        edittext = (EditText) view.findViewById(R.id.editText1);
+        final SeekBar seekbar = (SeekBar) view.findViewById(R.id.seekBar1);
+        seekbar.setOnSeekBarChangeListener(this);
+
+        final Button button = (Button) view.findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                buttonClicked(v);
+            }
+        });
+
+        return  view;
+    }
+
+    public void buttonClicked(View view) {
+        activityCallback.onButtonClick(seekvalue, edittext.getText().toString());
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        seekvalue = progress;
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
+}
