2b2d0ca75771e3f8c58d059100bc62f2c46ba597
DevInside <kecondev@gmail.com>
2017-01-30 13:44:27 +0900

    Processing...

--- FabExample/app/src/main/java/kr/co/devinside/fabexample/FabExampleActivity.java
+++ FabExample/app/src/main/java/kr/co/devinside/fabexample/FabExampleActivity.java
@@ -5,11 +5,21 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class FabExampleActivity extends AppCompatActivity {
+
+    ArrayList<String> listItems = new ArrayList<String>();
+    ArrayAdapter<String> adapter;
+    private ListView myListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +28,18 @@ public class FabExampleActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        myListView = (ListView) findViewById(R.id.listView);
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listItems);
+        myListView.setAdapter(adapter);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                addListItem();
+                Snackbar.make(view, "Item added to list", Snackbar.LENGTH_LONG)
+                        .setAction("Undo", undoOnClickListener).show();
             }
         });
     }
@@ -49,4 +65,19 @@ public class FabExampleActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private void addListItem() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss MM/dd/yyyy", Locale.US);
+        listItems.add(dateFormat.format(new java.util.Date()));
+        adapter.notifyDataSetChanged();
+    }
+
+    View.OnClickListener undoOnClickListener = new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+          listItems.remove(listItems.size() -1);
+          adapter.notifyDataSetChanged();
+          Snackbar.make(view, "Item removed", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+      }
+    };
 }
