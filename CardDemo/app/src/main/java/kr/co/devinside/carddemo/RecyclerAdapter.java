6b122b52c900d3572fc0c7d26aee9cb6fc5ebb8e
DevInside <kecondev@gmail.com>
2017-02-05 13:42:49 +0900

    Create

--- CardDemo/app/src/main/java/kr/co/devinside/carddemo/RecyclerAdapter.java
+++ CardDemo/app/src/main/java/kr/co/devinside/carddemo/RecyclerAdapter.java
@@ -1,8 +1,12 @@
 package kr.co.devinside.carddemo;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.widget.RecyclerView;
-
-import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by DevInside on 2017. 2. 5..
@@ -17,4 +21,47 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
 
     private int[] images = {R.drawable.android_image_1, R.drawable.android_image_2, R.drawable.android_image_3, R.drawable.android_image_4,
                             R.drawable.android_image_5, R.drawable.android_image_6, R.drawable.android_image_7, R.drawable.android_image_8 };
+
+
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.card_layout, viewGroup, false);
+        ViewHolder viewHolder = new ViewHolder(v);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int i) {
+        viewHolder.itemTitle.setText(titles[i]);
+        viewHolder.itemDetail.setText(details[i]);
+        viewHolder.itemImage.setImageResource(images[i]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return titles.length;
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView itemImage;
+        public TextView itemTitle;
+        public TextView itemDetail;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            itemImage = (ImageView)itemView.findViewById(R.id.item_image);
+            itemTitle = (TextView)itemView.findViewById(R.id.item_title);
+            itemDetail = (TextView)itemView.findViewById(R.id.item_detail);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition() + 1;
+
+                    Snackbar.make(v, "Click detected on item " + position, Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                }
+            });
+        }
+    }
 }
